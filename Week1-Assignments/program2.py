4e81bbf283cbc3c4dffb64b767bea7cf3b0cc7a8
Priya Shine <34813505+priyshi@users.noreply.github.com>
2025-09-06 20:00:55 +0530

    week1py2025

---
+++ Week1-Assignments/program2.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+experience = int(input("Enter your years of IT experience: "))
+
+if (age >= 22) and (experience >= 2):
+    print("Access Granted")
+else:
+    print("Access Denied")
---
+++ Week1-Assignments/program3.py
@@ -0,0 +1,4 @@
+employees = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+for i in range(len(employees)):
+    print(f"{i + 1}. {employees[i]}")   
